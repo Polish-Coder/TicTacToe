6fa8d1dc3b753e208360621d9055c14bafa55b5a
Polish Coder <polygonssoftware@gmail.com>
2024-01-01 04:10:21 +0100

    Added winning and drawing

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -7,12 +7,13 @@
 using namespace std;
 
 string update_cross(int fields[]);
+int is_won(int fields[]);
 
 int main(int argc, char* argv[])
 {
     random_device rd;
     mt19937 generator(rd());
-    uniform_int_distribution<int> distribution(1, 9);
+    const uniform_int_distribution<> distribution(1, 9);
     
     int fields[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     
@@ -20,12 +21,24 @@ int main(int argc, char* argv[])
     string input;
 
     while (true)
-    {
+    {        
         cout << "\033[2J\n";
 
         cross = update_cross(fields);
         
         cout << cross;
+
+        if (is_won(fields) != 0)
+        {
+            break;
+        }
+        
+        if (all_of(fields, fields + size(fields), [](const int value) { return value != 0; }))
+        {
+            cout << "Brak mozliwych ruchow, nikt nie wygral";
+            return 0;
+        }
+        
         cout << "Wpisz numer pola\n";
         
         cin >> input;
@@ -45,6 +58,18 @@ int main(int argc, char* argv[])
         cross = update_cross(fields);
 
         cout << cross;
+
+        if (is_won(fields) != 0)
+        {
+            break;
+        }
+
+        if (all_of(fields, fields + size(fields), [](const int value) { return value != 0; }))
+        {
+            cout << "Brak mozliwych ruchow, nikt nie wygral";
+            return 0;
+        }
+        
         cout << "Przeciwnik wybiera... ";
 
         this_thread::sleep_for(chrono::seconds(1));
@@ -61,6 +86,9 @@ int main(int argc, char* argv[])
 
         this_thread::sleep_for(chrono::seconds(1));
     }
+
+    const string winner = is_won(fields) == 1 ? "gracz" : "bot";
+    cout << "Wygrywa " + winner;
 
     return 0;
 }
@@ -110,4 +138,55 @@ string update_cross(int fields[])
     }
     
     return cross;
+}
+
+int is_won(int fields[])
+{
+    // Horizontal
+    
+    if (fields[0] != 0 && fields[0] == fields[1] && fields[1] == fields[2])
+    {
+        return fields[0];
+    }
+
+    if (fields[3] != 0 && fields[3] == fields[4] && fields[4] == fields[5])
+    {
+        return fields[3];
+    }
+
+    if (fields[6] != 0 && fields[6] == fields[7] && fields[7] == fields[8])
+    {
+        return fields[6];
+    }
+
+    // Vertical
+
+    if (fields[0] != 0 && fields[0] == fields[3] && fields[3] == fields[6])
+    {
+        return fields[0];
+    }
+
+    if (fields[1] != 0 && fields[1] == fields[4] && fields[4] == fields[7])
+    {
+        return fields[1];
+    }
+
+    if (fields[2] != 0 && fields[2] == fields[5] && fields[5] == fields[8])
+    {
+        return fields[2];
+    }
+
+    // Diagonal
+
+    if (fields[0] != 0 && fields[0] == fields[4] && fields[4] == fields[8])
+    {
+        return fields[0];
+    }
+
+    if (fields[2] != 0 && fields[2] == fields[4] && fields[4] == fields[6])
+    {
+        return fields[2];
+    }
+
+    return 0;
 }
